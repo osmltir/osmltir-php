8bc0013601c4369ab34c0195459b5c7c127e06dd
osmltir <ldelevallez@gmail.com>
2013-04-18 10:36:43 +0200

    Création php

--- index.php
+++ index.php
@@ -1,17 +1,67 @@
-<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr"><head>
+	    <title>OSM Lomme Tir</title>
+	    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<link rel="stylesheet" media="screen" type="text/css" href="style1.css">
 
-<html>
-	<head>
-        <meta charset="utf-8" />
-		<link rel="stylesheet" href="style.css"/>
-        <title>OSML Tir</title>
-		<link rel="icon" href="img/favicon.ico" type="image/ico" />
-	</head>
-
-    <body>
-		<header>
-            <?php include("entete.php"); ?>
-            <?php include("menu.php"); ?>
+		<script type="text/javascript">
+		sfHover = function() {
+		var sfEls = document.getElementById("menu").getElementsByTagName("LI");
+		for (var i=0; i<sfEls.length; i++) {
+			sfEls[i].onmouseover=function() {
+				this.className+="sfhover";
+			}
+			sfEls[i].onmouseout=function() {
+				this.className=this.className.replace(new RegExp("sfhover\\b"), "");
+			}
+		}
+		}
+		if (window.attachEvent) window.attachEvent("onload", sfHover);
+		</script>
+            <style type="text/css"></style></head>
+<body>
+    <br />
+        <header>
+            <img src="img/Header-osml.png" alt="Bannière Site OSML Tir">
         </header>
-    </body>
+        <br />
+            <div style="text-align : center;">
+        <ul id="menu">
+                <li><a href="#">accueil</a></li>
+                <li><a href="#">Qui sommes nous ?</a>
+                    <ul>
+			<li><a href="#">Installation</a></li>
+			<li><a href="#">Historique</a></li>
+			<li><a href="#">Comité Directeur</a></li>
+			<li><a href="#">Venir chez nous</a></li>
+                    </ul>
+		</li>
+                <li><a href="#">images</a>
+                    <ul>
+                        <li><a href="#">photos</a>
+                            <ul>
+				<li><a href="#">catégorie 1</a></li>
+				<li><a href="#">catégorie 2</a></li>
+                            </ul>
+                        </li>
+			<li><a href="#">vidéos</a></li>
+                    </ul>
+                </li>
+		<li><a href="#">Calendrier & Résultats</a>
+                    <ul>
+                        <li><a href="#">Calendriers</a></li>
+			<li><a href="#">Résultats</a></li>
+                    </ul>
+		</li>
+                <li><a href="#">plus</a>
+                    <ul>
+			<li><a href="#">forum</a></li>
+			<li><a href="#">liens</a></li>
+			<li><a href="#">nous contacter</a></li>
+			<li><a href="#">recherche</a></li>
+                    </ul>
+		</li>
+        </ul></div>
+        <br />
+        
+	</body>
 </html>
