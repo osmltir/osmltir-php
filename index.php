f765c356f33882d6c8b0f730202c02850ef62ef4
osmltir <ldelevallez@gmail.com>
2013-04-19 14:25:49 +0200

    ajout page letirsportif.

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
                         <div class="question"> Vous souhaitez ? </div>
                     </article>
                     <section>
-                        <a href="page1.html"> <div class="carre" id="carre1"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> le tir sportif !</div> </a>
-                        <a href="page2.html"> <div class="carre" id="carre2"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> l'OSML Tir !</div> </a>
-                        <a href="page3.html"> <div class="carre" id="carre3"> <br/> <br/> <br/> <br/>Vous rendre <br/> à l'OSML Tir ! </div> </a>
+                        <a href="letirsportif.php"> <div class="carre" id="carre1"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> le tir sportif !</div> </a>
+                        <a href="adherent.php"> <div class="carre" id="carre2"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> l'OSML Tir !</div> </a>
+                        <a href="nous.php"> <div class="carre" id="carre3"> <br/> <br/> <br/> <br/>Vous rendre <br/> à l'OSML Tir ! </div> </a>
                         <a href="contact.php"> <div class="carre" id="carre4"> <br/> <br/> <br/> <br/>Contacter <br/> l'OSML Tir !</div> </a>
                     </section>
 		</body>
