b7b341360c228daea44b1dc5397617922523e3ea
osmltir <ldelevallez@gmail.com>
2013-04-19 09:13:40 +0200

    Création php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css"/>
+        <title>OSML Tir</title>
+		<link rel="icon" href="img/favicon.ico" type="image/ico" />
+	</head>
+
+		<body>
+			<header>
+                            <?php include("entete.php"); ?>
+                            <h1> Vous êtes ? </h1>
+			</header>
+		
+			<section>
+				<a href="page1.html"> <div class="carre" id="carre1"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> le tir sportif !</div> </a>
+				<a href="page2.html"> <div class="carre" id="carre2"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> l'OSML Tir !</div> </a>
+				<a href="page3.html"> <div class="carre" id="carre3"> <br/> <br/> <br/> <br/>Te rendre <br/> à l'OSML Tir ! </div> </a>
+				<a href="contact.html"> <div class="carre" id="carre4"> <br/> <br/> <br/> <br/>Contacter <br/> l'OSML Tir !</div> </a>
+			</section>
+
+		</body>
+</html>
