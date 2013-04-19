cca5530fd1aaa811b465b8bd2c939689bfa34247
osmltir <ldelevallez@gmail.com>
2013-04-19 14:10:34 +0200

    Modification style question

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
         <title>OSML Tir</title>
 		<link rel="icon" href="img/favicon.ico" type="image/ico" />
 	</head>
-
 		<body>
                     <header>
                         <?php include("entete.php"); ?>
@@ -15,11 +14,11 @@
                     <article>
                         <div class="question"> Vous souhaitez ? </div>
                     </article>
-			<section>
-                            <a href="page1.html"> <div class="carre" id="carre1"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> le tir sportif !</div> </a>
-                            <a href="page2.html"> <div class="carre" id="carre2"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> l'OSML Tir !</div> </a>
-                            <a href="page3.html"> <div class="carre" id="carre3"> <br/> <br/> <br/> <br/>Vous rendre <br/> à l'OSML Tir ! </div> </a>
-                            <a href="contact.php"> <div class="carre" id="carre4"> <br/> <br/> <br/> <br/>Contacter <br/> l'OSML Tir !</div> </a>
-			</section>
+                    <section>
+                        <a href="page1.html"> <div class="carre" id="carre1"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> le tir sportif !</div> </a>
+                        <a href="page2.html"> <div class="carre" id="carre2"> <br/> <br/> <br/> <br/> <br/>Découvrir <br/> l'OSML Tir !</div> </a>
+                        <a href="page3.html"> <div class="carre" id="carre3"> <br/> <br/> <br/> <br/>Vous rendre <br/> à l'OSML Tir ! </div> </a>
+                        <a href="contact.php"> <div class="carre" id="carre4"> <br/> <br/> <br/> <br/>Contacter <br/> l'OSML Tir !</div> </a>
+                    </section>
 		</body>
 </html>
